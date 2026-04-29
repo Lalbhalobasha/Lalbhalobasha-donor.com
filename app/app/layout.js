caf46e77bf6ed4c8624c0e53c666cc4058fa9b05
Lalbhalobasha <alomgirhossain9820@gmail.com>
2026-04-30 01:21:10 +0600

    Create layout.js

---
+++ app/app/layout.js
@@ -0,0 +1,12 @@
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <head>
+        <title>Lalbhalobasha Donor</title>
+      </head>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
