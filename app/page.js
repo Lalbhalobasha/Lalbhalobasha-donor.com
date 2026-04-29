4498d1ce722c4656ab3f741df8b3fac7d165c29e
Lalbhalobasha <alomgirhossain9820@gmail.com>
2026-04-30 01:19:27 +0600

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>🩸 Lalbhalobasha Blood Donor</h1>
+      <p>রক্ত খুঁজুন, জীবন বাঁচান ❤️</p>
+
+      <button style={{
+        padding: "10px 20px",
+        background: "red",
+        color: "white",
+        border: "none",
+        borderRadius: "5px"
+      }}>
+        Donor খুঁজুন
+      </button>
+    </div>
+  );
+}
